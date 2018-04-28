e51d5c87e093a2e4201d4934030a348f5fb75070
EshaFerdous <eshaniloy@gmail.com>
2018-04-29 01:02:02 +0600

    Add files via upload

---
+++ app/src/main/java/example/com/messageretrieve/ShowCost.java
@@ -0,0 +1,13 @@
+package example.com.messageretrieve;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ShowCost extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_cost);
+    }
+}
